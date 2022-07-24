98212ebd1677dbe12ba6f0bd1c4fe98729f54d1d
Roman Kolesnev <rkolesnev@confluent.io>
2022-07-24 18:10:13 +0300

    Remove ExtendedConsumerInstrumentation as not needed until clusterId capture

--- instrumentation/kafka-clients/src/main/java/io/confluent/csid/data/governance/lineage/opentel/extension/kafkaclients/ExtendedKafkaConsumerRecordIteratorInstrumentation.java
+++ instrumentation/kafka-clients/src/main/java/io/confluent/csid/data/governance/lineage/opentel/extension/kafkaclients/ExtendedKafkaConsumerRecordIteratorInstrumentation.java
@@ -14,7 +14,7 @@ import io.confluent.csid.data.governance.lineage.opentel.extension.kafkaclients.
 import io.confluent.csid.data.governance.lineage.opentel.extension.kafkaclients.helpers.HeaderCapturingIterator;
 import io.confluent.csid.data.governance.lineage.opentel.extension.kafkaclients.helpers.HeaderCapturingList;
 import io.confluent.csid.data.governance.lineage.opentel.extension.kafkacommon.ServiceMetadata;
-import io.opentelemetry.instrumentation.api.field.VirtualField;
+import io.confluent.csid.data.governance.lineage.opentel.extension.kafkacommon.ServiceNameHolder;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import io.opentelemetry.javaagent.instrumentation.kafkaclients.ConsumerRecordsInstrumentation;
@@ -77,10 +77,8 @@ public class ExtendedKafkaConsumerRecordIteratorInstrumentation implements TypeI
         @Advice.This ConsumerRecords<K, V> consumerRecords,
         @Advice.Return(readOnly = false) Iterable<ConsumerRecord<K, V>> iterable) {
       if (iterable != null) {
-        VirtualField<ConsumerRecords<?, ?>, ServiceMetadata> consumerRecordsServiceMetadata =
-            VirtualField.find(ConsumerRecords.class, ServiceMetadata.class);
         iterable = HeaderCapturingIterable.wrap(iterable,
-            consumerRecordsServiceMetadata.get(consumerRecords));
+            new ServiceMetadata(ServiceNameHolder.get()));
       }
     }
   }
@@ -93,11 +91,8 @@ public class ExtendedKafkaConsumerRecordIteratorInstrumentation implements TypeI
         @Advice.This ConsumerRecords<K, V> consumerRecords,
         @Advice.Return(readOnly = false) List<ConsumerRecord<K, V>> list) {
       if (list != null) {
-        VirtualField<ConsumerRecords<?, ?>, ServiceMetadata> consumerRecordsServiceMetadata =
-            VirtualField.find(ConsumerRecords.class, ServiceMetadata.class);
-
         list = HeaderCapturingList.wrap(list,
-            consumerRecordsServiceMetadata.get(consumerRecords));
+            new ServiceMetadata(ServiceNameHolder.get()));
       }
     }
   }
@@ -110,10 +105,8 @@ public class ExtendedKafkaConsumerRecordIteratorInstrumentation implements TypeI
         @Advice.This ConsumerRecords<K, V> consumerRecords,
         @Advice.Return(readOnly = false) Iterator<ConsumerRecord<K, V>> iterator) {
       if (iterator != null) {
-        VirtualField<ConsumerRecords<?, ?>, ServiceMetadata> consumerRecordsServiceMetadata =
-            VirtualField.find(ConsumerRecords.class, ServiceMetadata.class);
         iterator = HeaderCapturingIterator.wrap(iterator,
-            consumerRecordsServiceMetadata.get(consumerRecords));
+            new ServiceMetadata(ServiceNameHolder.get()));
       }
     }
   }
