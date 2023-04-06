f3a2db5eeb908e8fa7d47f46552e351e37f94f09
ConfluentSemaphore <40306929+ConfluentSemaphore@users.noreply.github.com>
2023-04-06 08:36:52 +0000

    Squashed 'mk-include/' content from commit 36d27a1
    git-subtree-dir: mk-include
    git-subtree-split: 36d27a186de379a45b59e7e749b8247e1b7d6295

---
+++ cc-sonarqube.mk
@@ -0,0 +1,31 @@
+SONAR_AUTH_TOKEN_VAULT_KV := token
+SONAR_HOST := http://sonarqube.dp.confluent.io:9000
+SONAR_SCANNER_ARGS := -X -Dsonar.host.url=$(SONAR_HOST)
+
+SONAR_PROJECT_NAME := $(SEMAPHORE_PROJECT_NAME)
+
+ifeq ($(SEMAPHORE_GIT_PR_NUMBER),)
+	SONAR_SCANNER_ARGS += -Dsonar.branch.name=$(SEMAPHORE_GIT_BRANCH)
+else
+	SONAR_SCANNER_ARGS += -Dsonar.pullrequest.key=$(SEMAPHORE_GIT_PR_NUMBER)
+	SONAR_SCANNER_ARGS += -Dsonar.pullrequest.branch=$(SEMAPHORE_GIT_PR_BRANCH)
+	SONAR_SCANNER_ARGS += -Dsonar.pullrequest.base=$(SEMAPHORE_GIT_BRANCH)
+endif
+
+
+.PHONY: sonar-scan
+sonar-scan:
+	@sonar-scanner $(SONAR_SCANNER_ARGS) -Dsonar.login=$(shell vault kv get -field $(SONAR_AUTH_TOKEN_VAULT_KV) "v1/ci/kv/sonarqube/semaphore")
+
+.PHONY: sonarqube-gate-pip-deps
+sonarqube-gate-pip-deps:
+	pip3 show confluent-ci-tools > /dev/null || pip3 install -U confluent-ci-tools
+
+
+.PHONY: sonar-gate
+sonar-gate: sonarqube-gate-pip-deps
+	@sonarqube-ci gate \
+		$(SONAR_PROJECT_NAME) \
+		--pr-id $(SEMAPHORE_GIT_PR_NUMBER) \
+		--token $(shell vault kv get -field $(SONAR_AUTH_TOKEN_VAULT_KV) "v1/ci/kv/sonarqube/semaphore") \
+		--host $(SONAR_HOST)
